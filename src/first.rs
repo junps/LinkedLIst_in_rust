7d687657bfa9a0e4c12ed38e9d0d5eccdfccd6e6
“junps” <junpppsuzuki@gmail.com>
2023-02-20 13:48:08 +0900

    add push function

--- src/first.rs
+++ src/first.rs
@@ -1,16 +1,18 @@
-#[derive(Debug)]
+use std::mem;
+
+#[derive(Debug, PartialEq, Eq)]
 struct Node {
     elem: i32,
     next: List,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum Link {
     Empty,
     More(Box<Node>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct List {
     head: Link,
 }
@@ -19,23 +21,32 @@ impl List {
     pub fn new() -> Self {
         Self { head: Link::Empty }
     }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_next = List {
+            head: mem::replace(&mut self.head, Link::Empty),
+        };
+        let new_node = Box::new(Node {
+            elem,
+            next: new_next,
+        });
+        self.head = Link::More(new_node);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn print() {
-        let l1 = List { head: Link::Empty };
-        let l2 = List {
+    fn push() {
+        let mut l1 = List::new();
+        l1.push(100);
+        let expected_list = List {
             head: Link::More(Box::new(Node {
-                elem: 1,
+                elem: 100,
                 next: List { head: Link::Empty },
             })),
         };
-        println!("{:?}", l1);
-        println!("{:?}", l2);
-        let l3 = List::new();
-        println!("{:?}", l3);
+        assert_eq!(l1, expected_list);
     }
 }
