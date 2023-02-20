82c70fb0c48375263368255acc57daf04245b925
“junps” <junpppsuzuki@gmail.com>
2023-02-20 15:27:21 +0900

    implement Drop for List(if not, stack overflow will happen)

--- src/first.rs
+++ src/first.rs
@@ -41,6 +41,15 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -64,5 +73,15 @@ mod tests {
         l1.push(100);
         assert_eq!(l1.pop(), Some(100));
         assert_eq!(l1, List::new());
+    }
+
+    #[test]
+    fn drop() {
+        // if you comment out the Drop implementation for List, this code will cause stack overflow
+        // because the default drop will be done recursively.
+        let mut list = List::new();
+        for _ in 0..100000 {
+            list.push(0);
+        }
     }
 }
